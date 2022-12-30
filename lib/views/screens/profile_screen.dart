c1d2406cbddfca7c11edaf3c0b9ec8f9a22f53c0
Javokhir Jurakhonov <49615839+helloawesomeprojects@users.noreply.github.com>
2022-12-31 01:27:17 +0500

    Update profile_screen.dart

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:moodvi/constants.dart';
 import 'package:moodvi/views/widgets/custom_icon.dart';
+lass DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({Key? key}) : super(key: key);
+
+  showOptionsDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(children: [
+        SimpleDialogOption(
+          onPressed: ()  {},
+          child: Row(
+            children: const [
+              Icon(Icons.image),
+              Padding(
+                padding: EdgeInsets.all(7.0),
+                child: Text(
+                  ' Gallery',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+
+      ]),
+    );
+  }
+
+  
